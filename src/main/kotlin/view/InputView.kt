b0dc77a61a3d3f8e6cff0165b2f02cc6491c82e3
콜트 <qowoghd@gmail.com>
2022-01-23 18:18:38 +0900

    feat(view) : add input tryCount feature

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -8,3 +8,12 @@ fun inputCarNames(): String {
 }
 
 private fun pleaseInputCarNames() = println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)")
+
+fun inputTryCount(): Int {
+    pleaseInputTryCount()
+    val tryCount = readLine()?.toIntOrNull()
+    requireNotNull(tryCount)
+    return tryCount
+}
+
+private fun pleaseInputTryCount() = println("시도할 횟수는 몇회인가요?")
