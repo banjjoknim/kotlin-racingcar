b9639e807eb0dfecaa9d177732f08a482743b1b0
콜트 <qowoghd@gmail.com>
2022-01-23 17:55:04 +0900

    refactor(view) : refactor input car names logic

--- src/main/kotlin/view/InputView.kt
+++ src/main/kotlin/view/InputView.kt
@@ -1,9 +1,10 @@
 package view
 
-fun pleaseInputCarNames() = println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)")
-
 fun inputCarNames(): String {
+    pleaseInputCarNames()
     val carNames = readLine()
     require(!carNames.isNullOrBlank())
     return carNames
 }
+
+private fun pleaseInputCarNames() = println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분)")
