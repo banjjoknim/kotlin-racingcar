775639a8e0253e1ed683e0fd264856cb16596ae4
콜트 <qowoghd@gmail.com>
2022-01-23 18:53:11 +0900

    refactor(model) : add const val in class Car

--- src/main/kotlin/model/Car.kt
+++ src/main/kotlin/model/Car.kt
@@ -1,3 +1,7 @@
 package model
 
-class Car(val carName: CarName, val position: Int = 0)
+class Car(val carName: CarName, val position: Int = CAR_INITIAL_POSITION) {
+    companion object {
+        const val CAR_INITIAL_POSITION = 0
+    }
+}
