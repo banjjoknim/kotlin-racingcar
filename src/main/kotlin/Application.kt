fe4d1b51ca22aec4520f51e906165db6fb547bff
콜트 <qowoghd@gmail.com>
2022-01-26 03:09:15 +0900

    refactor : declare extension function in class Car

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -3,19 +3,18 @@ import model.CarNames
 import model.Cars
 import model.RacingGame
 import model.TryCount
+import model.randomMovableFunction
 import view.inputCarNames
 import view.inputTryCount
 import view.printCarsStatus
 import view.printResult
-import kotlin.random.Random
 
 fun main() {
     val cars = Cars(CarNames(inputCarNames()))
     val tryCount = TryCount(inputTryCount())
-    val isMovableFunction = { Random.nextInt(10) >= 4 }
     var racingGame = RacingGame(cars)
     repeat(tryCount.value) {
-        racingGame.run(isMovableFunction)
+        racingGame.run(randomMovableFunction())
         printCarsStatus(racingGame.cars)
     }
     val winners = racingGame.findWinners()
