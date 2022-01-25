069a6960a310e21474f50d133363214e9c2170ee
콜트 <qowoghd@gmail.com>
2022-01-26 02:06:32 +0900

    feat(model) : add function findWinners() in class Cars

--- src/main/kotlin/model/Cars.kt
+++ src/main/kotlin/model/Cars.kt
@@ -10,6 +10,16 @@ class Cars(private val _cars: List<Car>) {
         _cars.forEach { car -> car.move(movableFunction) }
     }
 
+    fun findWinners(): List<Car> {
+        val maxPosition = maxPosition()
+        return this._cars.filter { car -> car.isIn(maxPosition) }
+    }
+
+    private fun maxPosition(): Position {
+        return this._cars.map(Car::position).maxByOrNull(Position::value)
+            ?: throw NoSuchElementException("there is no element. because cars are empty.")
+    }
+
     val cars: List<Car>
         get() = ArrayList(_cars)
 }
