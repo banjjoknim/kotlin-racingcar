a04ccee61e06a779ee388ca2b9933ab2887b92b2
banjjoknim <zhfxmtkachdt@daum.net>
2022-02-06 02:22:36 +0900

    refactor(view, controller) : change print logic

--- src/main/kotlin/controller/RacingGame.kt
+++ src/main/kotlin/controller/RacingGame.kt
@@ -2,14 +2,12 @@ package controller
 
 import model.Cars
 import model.TryCount
-import view.printCarRecords
-import view.printWinner
+import view.printGameResult
 
 class RacingGame(private val _cars: Cars, private val _tryCount: TryCount) {
     fun run(movableFunction: () -> Boolean) {
         val carRecords = _cars.race(_tryCount, movableFunction)
-        printCarRecords(carRecords)
         val winners = _cars.findWinners()
-        printWinner(winners)
+        printGameResult(carRecords, winners)
     }
 }
--- src/main/kotlin/view/OutputView.kt
+++ src/main/kotlin/view/OutputView.kt
@@ -7,12 +7,17 @@ import model.CarRecords
 private const val CAR_POSITION_SYMBOL = "-"
 private const val WINNER_NAME_SEPARATOR = ","
 
-fun printWinner(cars: List<Car>) {
+fun printGameResult(carRecords: CarRecords, winners: List<Car>) {
+    printCarRecords(carRecords)
+    printWinner(winners)
+}
+
+private fun printWinner(cars: List<Car>) {
     val winnerNames = cars.joinToString(WINNER_NAME_SEPARATOR) { car -> car.carName.value }
     println("$winnerNames 이(가) 최종 우승하였습니다.")
 }
 
-fun printCarRecords(carRecords: CarRecords) {
+private fun printCarRecords(carRecords: CarRecords) {
     repeat(carRecords.size) { count ->
         carRecords.getCarRecords(count)
             .forEach(::printCarRecord)
